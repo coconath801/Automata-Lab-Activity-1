9775ba0e12d065e9e18ed175b69e9ddf5903578a
coconath801 <z.vadil.556575@umindanao.edu.ph>
2025-11-02 18:44:07 +0800

    Update act1.java

--- act1.java
+++ act1.java
@@ -6,11 +6,13 @@ import java.util.Scanner;
 public class act1 {
 	public static void main (String [] args) {
 		Scanner scan = new Scanner (System.in);
-		System.out.print("Enter a binary string:");
-		String str = scan.nextLine();
-		
-		
-		
+		    System.out.print("Enter a binary string (or type 'exit' to quit): ");
+		    String str = scan.nextLine();
+		    if (str.equalsIgnoreCase("exit")) {
+		        System.out.println("Program terminated.");
+		        break;
+		    }
+		    
 		String state = "q0";
 	for (int i = 0;  i < str.length(); i++) {
 		char symbol =str.charAt(i);
@@ -49,3 +51,4 @@ public class act1 {
 	
 
 }
+
